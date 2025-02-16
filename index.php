cee640b570504a7ede33060af5a72de06a765338
nim1236 <147692828+nim1236@users.noreply.github.com>
2025-02-16 21:18:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой сайт</title>
+    <link rel="stylesheet" href="osnova.css">
+</head>
+<body>
+    <img src="8.png" alt="Логотип" class="logo">
+    <img src="33.png" alt="logo-2" class="logo-2">
+    <div class="text-container">
+        <h1 class="text1">Добро пожаловать на мой сайт!</h1>
+        <p class="text">Здесь будет ваш контент.</p>
+    </div>
+</body>
+</html>
